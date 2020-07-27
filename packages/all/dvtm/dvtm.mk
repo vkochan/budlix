5080143a5326e519f583ec4ed530247aad00675b
Vadym Kochan <vadim4j@gmail.com>
2020-07-27 23:46:23 +0300

    packages: add dvtm package
    Add dvtm package which is alternative terminal multiplexer as tmux,
    but with smaller code base.
    
    Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

---
+++ packages/all/dvtm/dvtm.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# dvtm
+#
+################################################################################
+
+DVTM_VERSION = 311a8c0c28296f8f87fb63349e0f3254c7481e14
+DVTM_SITE = $(call github,martanne,dvtm,$(DVTM_VERSION))
+DVTM_DEPENDENCIES = ncurses
+
+define DVTM_BUILD_CMDS
+	$(BUILD_OPTS) $(MAKE) -C $(@D)
+endef
+
+define DVTM_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D) PREFIX=/usr DESTDIR=$(INSTALL_DIR) install
+endef
+
+$(eval $(generic-package))
