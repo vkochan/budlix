1ef4f567bba2f94164e4c9cb46289643ccc0622d
Vadym Kochan <vadim4j@gmail.com>
2020-07-30 02:35:40 +0300

    packages: iproute2: new package
    Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

--- mirrors.mk
+++ mirrors.mk
@@ -1,1 +1,2 @@
 GNU_MIRROR = http://ftpmirror.gnu.org
+KERNEL_MIRROR = https://cdn.kernel.org/pub
---
+++ packages/all/iproute2/iproute2.mk
@@ -0,0 +1,36 @@
+################################################################################
+#
+# iproute2
+#
+################################################################################
+
+IPROUTE2_VERSION = 5.6.0
+IPROUTE2_SOURCE = iproute2-$(IPROUTE2_VERSION).tar.xz
+IPROUTE2_SITE = $(KERNEL_MIRROR)/linux/utils/net/iproute2
+IPROUTE2_DEPENDENCIES = bison flex pkgconf
+IPROUTE2_LICENSE = GPL-2.0+
+IPROUTE2_LICENSE_FILES = COPYING
+
+define IPROUTE2_DISABLE_IPTABLES
+	# m_xt.so is built unconditionally
+	echo "TC_CONFIG_XT:=n" >>$(@D)/config.mk
+endef
+
+define IPROUTE2_CONFIGURE_CMDS
+	cd $(@D) && $(BUILD_MAKE_ENV) ./configure
+	$(IPROUTE2_DISABLE_IPTABLES)
+endef
+
+define IPROUTE2_BUILD_CMDS
+	$(BUILD_MAKE_ENV) LDFLAGS="$(BUILD_LDFLAGS)" \
+		CFLAGS="$(BUILD_CFLAGS) -DXT_LIB_DIR=\\\"/usr/lib/xtables\\\"" \
+		$(MAKE) V=1 LIBDB_LIBS=-lpthread \
+		DBM_INCLUDE="$(STAGING_DIR)/usr/include" \
+		SHARED_LIBS="y" -C $(@D)
+endef
+
+define IPROUTE2_INSTALL_CMDS
+	$(BUILD_MAKE_ENV) DESTDIR="$(INSTALL_DIR)" $(MAKE) -C $(@D) install
+endef
+
+$(eval $(generic-package))
