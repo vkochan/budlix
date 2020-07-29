4ff7adde30eb59f8e6f9120c9203a6ed9c61e992
Vadym Kochan <vadim4j@gmail.com>
2020-07-29 20:38:13 +0300

    packages: libtool: new package
    Libtool is needed for the autotools infra build system.
    
    Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

---
+++ packages/all/libtool/libtool.mk
@@ -0,0 +1,25 @@
+################################################################################
+#
+# libtool
+#
+################################################################################
+
+LIBTOOL_VERSION = 2.4.6
+LIBTOOL_SOURCE = libtool-$(LIBTOOL_VERSION).tar.xz
+LIBTOOL_SITE = $(GNU_MIRROR)/libtool
+
+# LIBTOOL_SUBDIR = libltdl
+LIBTOOL_SUBDIR = .
+
+LIBTOOL_CONF_OPTS = --enable-ltdl-install
+
+LIBTOOL_DEPENDENCIES = m4 help2man
+
+LIBTOOL_LICENSE = LGPL-2.1+
+LIBTOOL_LICENSE_FILES = $(LIBTOOL_SUBDIR)/COPYING.LIB
+
+$(eval $(autotools-package))
+
+# variables used by other packages
+LIBTOOL = $(STAGING_DIR)/usr/bin/libtool
+LIBTOOLIZE = $(STAGING_DIR)/usr/bin/libtoolize
