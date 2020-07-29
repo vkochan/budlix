bbb12b37ffd38b9a53eec9bbf3a543a41f65a5e5
Vadym Kochan <vadim4j@gmail.com>
2020-07-30 02:36:05 +0300

    packages: flex: new package
    Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

---
+++ packages/all/flex/flex.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# flex
+#
+################################################################################
+
+FLEX_VERSION = 2.6.4
+FLEX_SITE = https://github.com/westes/flex/files/981163
+FLEX_LICENSE = FLEX
+FLEX_LICENSE_FILES = COPYING
+FLEX_DEPENDENCIES = m4
+
+$(eval $(autotools-package))
