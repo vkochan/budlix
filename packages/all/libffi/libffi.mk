1af35b9fa621e968ab20a08d20b1325492253a84
Vadym Kochan <vadim4j@gmail.com>
2020-07-30 00:46:56 +0300

    packages: libffi: new package
    Libffi can be used in such programs to provide a bridge from the
    interpreter program to compiled code. It is needed for python package.
    
    Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

---
+++ packages/all/libffi/libffi.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# libffi
+#
+################################################################################
+
+LIBFFI_VERSION = 3.3
+LIBFFI_SITE = $(call github,libffi,libffi,v$(LIBFFI_VERSION))
+LIBFFI_LICENSE = MIT
+LIBFFI_LICENSE_FILES = LICENSE
+LIBFFI_INSTALL_STAGING = YES
+LIBFFI_AUTORECONF = YES
+
+$(eval $(autotools-package))
