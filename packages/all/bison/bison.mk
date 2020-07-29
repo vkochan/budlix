05bc0c4fd36701359ffc0f7bbffe40873b19e6cb
Vadym Kochan <vadim4j@gmail.com>
2020-07-30 02:33:58 +0300

    packages: bison: new package
    Bison is a general-purpose parser generator that converts an annotated
    context-free grammar into a deterministic LR or generalized LR (GLR)
    parser employing LALR(1) parser tables
    
    Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

---
+++ packages/all/bison/bison.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# bison
+#
+################################################################################
+
+BISON_VERSION = 3.4.2
+BISON_SOURCE = bison-$(BISON_VERSION).tar.xz
+BISON_SITE = $(GNU_MIRROR)/bison
+BISON_LICENSE = GPL-3.0+
+BISON_LICENSE_FILES = COPYING
+BISON_DEPENDENCIES = m4
+
+$(eval $(autotools-package))
