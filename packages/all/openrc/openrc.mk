922e9e221abef68e629283f251edcd711c32629a
Vadym Kochan <vadim4j@gmail.com>
2020-07-30 14:07:50 +0300

    packages: openrc: new package
    Add openrc as possible default init system.
    
    Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

---
+++ packages/all/openrc/openrc.mk
@@ -0,0 +1,37 @@
+################################################################################
+#
+# openrc
+#
+################################################################################
+
+OPENRC_VERSION = 0.42.1
+OPENRC_SITE = $(call github,OpenRC,openrc,$(OPENRC_VERSION))
+OPENRC_LICENSE = BSD-2-Clause
+OPENRC_LICENSE_FILES = LICENSE
+
+OPENRC_DEPENDENCIES = ncurses
+
+# set LIBNAME so openrc puts files in proper directories and sets proper
+# paths in installed files. Since in buildroot /lib64 and /lib32 always
+# points to /lib, it's safe to hardcode it to "lib"
+OPENRC_MAKE_OPTS = \
+	LIBNAME=lib \
+	LIBEXECDIR=$(INSTALL_DIR)/usr/libexec/rc \
+	PREFIX=$(INSTALL_DIR) \
+	MKPKGCONFIG=no \
+	MKSYSVINIT=yes \
+	BRANDING="NoneLinux" \
+	PKG_PREFIX=$(INSTALL_DIR) \
+	CC=$(BUILD_CC)
+
+define OPENRC_BUILD_CMDS
+	$(BUILD_MAKE_ENV) $(MAKE) $(OPENRC_MAKE_OPTS) -C $(@D)
+endef
+
+define OPENRC_INSTALL_CMDS
+	$(BUILD_MAKE_ENV) $(MAKE) $(OPENRC_MAKE_OPTS) -C $(@D) install
+	$(INSTALL) -D -m 0755 $(OPENRC_PKGDIR)/sysv-rcs \
+		$(INSTALL_DIR)/etc/init.d/sysv-rcs
+endef
+
+$(eval $(generic-package))
