81d03fe5db5c6be4ef6d70c1f0cf548536f818d3
Vadym Kochan <vadim4j@gmail.com>
2020-08-01 12:02:33 +0300

    packages: lua: new package
    Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

---
+++ packages/all/lua/lua.mk
@@ -0,0 +1,39 @@
+################################################################################
+#
+# lua
+#
+################################################################################
+
+LUA_VERSION = 5.3.5
+LUA_SITE = http://www.lua.org/ftp
+LUA_LICENSE = MIT
+LUA_DEPENDENCIES = readline ncurses
+
+LUA_CFLAGS = -Wall -fPIC -DLUA_USE_POSIX
+LUA_CFLAGS += -DLUA_USE_DLOPEN
+LUA_BUILDMODE = dynamic
+LUA_MYLIBS += -ldl
+
+define LUA_LUACONF
+	$(SED) 's|#define LUA_ROOT.*|#define LUA_ROOT "$(STAGING_DIR)/usr/"|' $(@D)/src/luaconf.h
+endef
+LUA_POST_PATCH_HOOKS += LUA_LUACONF
+
+define LUA_BUILD_CMDS
+	$(BUILD_MAKE_ENV) $(MAKE) \
+	CFLAGS="$(LUA_CFLAGS)" \
+	MYLDFLAGS="$(BUILD_LDFLAGS)" \
+	MYLIBS="$(LUA_MYLIBS)" \
+	BUILDMODE=$(LUA_BUILDMODE) \
+	PKG_VERSION=$(LUA_VERSION) -C $(@D)/src all
+	sed -e "s/@VERSION@/$(LUA_VERSION)/;s/@ABI@/$(LUAINTERPRETER_ABIVER)/;s/@MYLIBS@/$(LUA_MYLIBS)/" \
+		$(LUA_PKGDIR)/lua.pc.in > $(@D)/lua.pc
+endef
+
+define LUA_INSTALL_CMDS
+	$(BUILD_MAKE_ENV) $(MAKE) INSTALL_TOP="$(INSTALL_DIR)/usr" -C $(@D) install
+	$(INSTALL) -m 0644 -D $(@D)/lua.pc \
+		$(INSTALL_DIR)/usr/lib/pkgconfig/lua.pc
+endef
+
+$(eval $(generic-package))
