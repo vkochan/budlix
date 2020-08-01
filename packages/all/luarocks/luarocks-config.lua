1eca08aed48946c32c1af18354782698ff26993a
Vadym Kochan <vadim4j@gmail.com>
2020-08-01 23:46:10 +0300

    packages: luarocks: handle config file more properly
    Use $(STAGING_DIR)/etc/luarocks/config.lua for other packages,
    but use with $(INSTALL_DIR) for local installation.
    
    Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

---
+++ packages/all/luarocks/luarocks-config.lua
@@ -0,0 +1,4 @@
+local function getenv (name) return os_getenv(name) or '' end
+external_deps_dirs = { getenv('STAGING_DIR') .. [[/usr]] }
+gcc_rpath = false
+wrap_bin_scripts = false
--- packages/all/luarocks/luarocks.mk
+++ packages/all/luarocks/luarocks.mk
@@ -10,8 +10,13 @@ LUAROCKS_LICENSE = MIT
 LUAROCKS_LICENSE_FILES = COPYING
 LUAROCKS_DEPENDENCIES = lua
 
+LUAROCKS_CONFIG_DIR = $(STAGING_DIR)/etc
+LUAROCKS_CONFIG_FILE_DEFAULT = $(INSTALL_DIR)/etc/luarocks/config-$(LUA_ABIVER).lua
+LUAROCKS_CONFIG_FILE = $(LUAROCKS_CONFIG_DIR)/luarocks/config.lua
+
 LUAROCKS_CONF_OPTS = \
 	--prefix=$(INSTALL_DIR)/usr \
+	--rocks-tree=$(STAGING_DIR)/usr \
 	--sysconfdir=$(INSTALL_DIR)/etc \
 	--with-lua=$(STAGING_DIR)/usr
 
@@ -20,7 +25,11 @@ define LUAROCKS_CONFIGURE_CMDS
 endef
 
 define LUAROCKS_INSTALL_CMDS
+	rm -f $(LUAROCKS_CONFIG_FILE_DEFAULT)
 	$(MAKE) -C $(@D) install
+	cat $(LUAROCKS_CONFIG_FILE_DEFAULT) $(LUAROCKS_PKGDIR)/luarocks-config.lua \
+		> $(INSTALL_DIR)/etc/luarocks/config.lua
+	rm -f $(LUAROCKS_CONFIG_FILE_DEFAULT)
 endef
 
 $(eval $(generic-package))
