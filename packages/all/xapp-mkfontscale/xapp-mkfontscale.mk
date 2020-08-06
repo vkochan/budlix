c9726aa1b524101575e0e9e75533adf6256976b7
Vadym Kochan <vadim4j@gmail.com>
2020-08-06 16:54:20 +0300

    packages: xapp-mkfontscale: new package
    Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

---
+++ packages/all/xapp-mkfontscale/xapp-mkfontscale.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# xapp-mkfontscale
+#
+################################################################################
+
+XAPP_MKFONTSCALE_VERSION = 1.2.1
+XAPP_MKFONTSCALE_SOURCE = mkfontscale-$(XAPP_MKFONTSCALE_VERSION).tar.bz2
+XAPP_MKFONTSCALE_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_MKFONTSCALE_LICENSE = MIT
+XAPP_MKFONTSCALE_LICENSE_FILES = COPYING
+XAPP_MKFONTSCALE_DEPENDENCIES = zlib freetype xlib-libfontenc xorgproto
+
+$(eval $(autotools-package))
