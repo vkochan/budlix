3a58fb2abc68ef30a90581337ef068570e291d8a
Vadym Kochan <vadim4j@gmail.com>
2020-08-07 11:54:19 +0300

    packages: xapp-xkbcomp: fix xkb root config dir
    The codes are installed by xkeyboard-config so we need point to
    $(STAGING_DIR).
    
    Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

--- packages/all/xapp-xkbcomp/xapp-xkbcomp.mk
+++ packages/all/xapp-xkbcomp/xapp-xkbcomp.mk
@@ -11,5 +11,6 @@ XAPP_XKBCOMP_LICENSE = MIT
 XAPP_XKBCOMP_LICENSE_FILES = COPYING
 XAPP_XKBCOMP_DEPENDENCIES = xlib-libX11 xlib-libxkbfile
 XAPP_XKBCOMP_CONF_ENV = ac_cv_file___xkbparse_c=yes
+XAPP_XKBCOMP_CONF_OPTS = --with-xkb_config_root=$(STAGING_DIR)/usr/share/X11/xkb
 
 $(eval $(autotools-package))
