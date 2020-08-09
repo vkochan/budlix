3db029454e4f309a4cb33b482c43987e65cc34e8
Vadym Kochan <vadim4j@gmail.com>
2020-08-09 13:31:03 +0300

    packages: eudev: remove unused _INSTALL_STAGING variable
    Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

--- packages/all/eudev/eudev.mk
+++ packages/all/eudev/eudev.mk
@@ -8,7 +8,6 @@ EUDEV_VERSION = 3.2.9
 EUDEV_SITE = http://dev.gentoo.org/~blueness/eudev
 EUDEV_LICENSE = GPL-2.0+ (programs), LGPL-2.1+ (libraries)
 EUDEV_LICENSE_FILES = COPYING
-EUDEV_INSTALL_STAGING = YES
 EUDEV_DEPENDENCIES = gperf pkgconf util-linux kmod
 EUDEV_PROVIDES = udev
 EUDEV_CONF_OPTS = \
