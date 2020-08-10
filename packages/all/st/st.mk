0a36cc823f52b79b12f3c52bc2cd5a24ec9b00be
Vadym Kochan <vadim4j@gmail.com>
2020-08-10 13:46:17 +0300

    packages: st: new package
    Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

---
+++ packages/all/st/st.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# st
+#
+################################################################################
+
+ST_VERSION = fa253f077f19b3220c7655b81bd91e52f4367803
+ST_SITE = git://git.suckless.org/st
+ST_DEPENDENCIES = xlib-libX11 xlib-libXft fontconfig freetype
+
+ST_OPTS = PREFIX=$(INSTALL_DIR)/usr \
+	  X11INC=$(STAGING_DIR)/usr/include/X11 \
+	  X11LIB=$(STAGING_DIR)/usr/lib
+
+define ST_BUILD_CMDS
+	$(MAKE) $(BUILD_OPTS) $(ST_OPTS) -C $(@D)
+endef
+
+define ST_INSTALL_CMDS
+	$(MAKE) $(BUILD_OPTS) $(ST_OPTS) -C $(@D) install
+endef
+
+$(eval $(generic-package))
