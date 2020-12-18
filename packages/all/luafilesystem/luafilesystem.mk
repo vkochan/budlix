121668d50d0698c8ba2eccf82ff4500cc2177b9a
Vadym Kochan <vadym.kochan@plvision.eu>
2020-12-18 18:42:16 +0200

    packages: new luafilesystem
    Signed-off-by: Vadym Kochan <vadym.kochan@plvision.eu>

---
+++ packages/all/luafilesystem/luafilesystem.mk
@@ -0,0 +1,11 @@
+################################################################################
+#
+# luafilesystem
+#
+################################################################################
+
+LUAFILESYSTEM_VERSION = 1.8.0-1
+LUAFILESYSTEM_SUBDIR = luafilesystem
+LUAFILESYSTEM_LICENSE = MIT
+
+$(eval $(luarocks-package))
