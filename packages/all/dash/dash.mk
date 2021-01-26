8f30443973a65b30f11cd91ce822ac886fe61b9c
Vadym Kochan <vadym.kochan@plvision.eu>
2021-01-26 18:55:11 +0200

    packages: dash: add libedit as dependency
    Signed-off-by: Vadym Kochan <vadym.kochan@plvision.eu>

--- packages/all/dash/dash.mk
+++ packages/all/dash/dash.mk
@@ -8,8 +8,8 @@ DASH_VERSION = 0.5.11
 DASH_SITE = http://gondor.apana.org.au/~herbert/dash/files
 DASH_LICENSE = BSD-3-Clause, GPL-2.0+ (mksignames.c)
 DASH_LICENSE_FILES = COPYING
-
-DASH_CONF_OPTS += --without-libedit
+DASH_DEPENDENCIES += libedit
+DASH_CONF_OPTS += --with-libedit
 
 define DASH_INSTALL_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/src/dash $(INSTALL_DIR)/usr/bin/dash
