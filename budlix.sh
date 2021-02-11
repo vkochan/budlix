875ac357ee873174d01746bc6e18f11ba71b1bdd
Vadym Kochan <vadym.kochan@plvision.eu>
2021-02-11 16:01:44 +0200

    env: pick all needed environment variables
    Signed-off-by: Vadym Kochan <vadym.kochan@plvision.eu>

--- budlix.sh
+++ budlix.sh
@@ -33,3 +33,16 @@ BLX_SYS_TERMINFO="${BLX_SYS_ROOT}/usr/share/terminfo"
 export PATH=${BLX_USER_PATH}:${BLX_SYS_PATH}:${PATH}
 export MANPATH=${BLX_USER_MANPATH}:${BLX_SYS_MANPATH}
 export TERMINFO=${BLX_USER_TERMINFO}:${BLX_SYS_TERMINFO}
+
+if [ -d ${BLX_SYS_PROFILE}/sysroot/etc/budlix/env ]; then
+    for e in ${BLX_SYS_PROFILE}/sysroot/etc/budlix/env/*; do
+        source $e
+    done
+fi
+if [ -d ${BLX_USER_PROFILE}/sysroot/etc/budlix/env ]; then
+    for e in ${BLX_USER_PROFILE}/sysroot/etc/budlix/env/*; do
+        source $e
+    done
+fi
+
+[ -e ${BLX_USER_PROFILE}/profile.sh ] && source ${BLX_USER_PROFILE}/profile.sh
