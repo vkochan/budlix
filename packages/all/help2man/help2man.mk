b148629ec898608c73acebd046abf968073da021
Vadym Kochan <vadim4j@gmail.com>
2021-05-25 15:01:39 +0300

    packages: help2man: use local built perl version
    Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

--- packages/all/help2man/help2man.mk
+++ packages/all/help2man/help2man.mk
@@ -8,6 +8,12 @@ HELP2MAN_VERSION = 1.47.16
 HELP2MAN_SOURCE = help2man-$(HELP2MAN_VERSION).tar.xz
 HELP2MAN_SITE = $(GNU_MIRROR)/help2man
 
+define HELP2MAN_FIXUP_PERL_PATH
+	$(SED) -e 's|#!/usr/bin/perl -w|#!/usr/bin/env perl|' $(INSTALL_DIR)/usr/bin/help2man
+endef
+
+HELP2MAN_POST_INSTALL_HOOKS += HELP2MAN_FIXUP_PERL_PATH
+
 $(eval $(autotools-package))
 
 HELP2MAN = $(STAGING_DIR)/usr/bin/help2man
