809161026d71321631640d2edc37dd23f54b9ef0
Vadym Kochan <vadim4j@gmail.com>
2021-05-26 15:51:01 +0300

    packages: help2man: remove perl path fixup
    Since perl prefix has full path the she-bang will contain
    it in the help2man script.
    
    Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

--- packages/all/help2man/help2man.mk
+++ packages/all/help2man/help2man.mk
@@ -7,12 +7,6 @@
 HELP2MAN_VERSION = 1.47.16
 HELP2MAN_SOURCE = help2man-$(HELP2MAN_VERSION).tar.xz
 HELP2MAN_SITE = $(GNU_MIRROR)/help2man
-
-define HELP2MAN_FIXUP_PERL_PATH
-	$(SED) -e 's|#!/usr/bin/perl -w|#!/usr/bin/env perl|' $(INSTALL_DIR)/usr/bin/help2man
-endef
-
-HELP2MAN_POST_INSTALL_HOOKS += HELP2MAN_FIXUP_PERL_PATH
 HELP2MAN_DEPENDENCIES = perl-locale-gettext
 HELP2MAN_USE_FOR_BUILD = YES
 
